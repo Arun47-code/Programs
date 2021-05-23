90ded84c4b49ec9ca21ef0644fd07da12732197e
Arun Borale <84652159+Arun47-code@users.noreply.github.com>
2021-05-23 11:21:26 +0530

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,8 @@
+
+
+def sortString(str1):
+	return ''.join(sorted(str1))	
+str1 = input("Enter the String")
+print("Number of elements present in given array: " + str(len(str1)));    
+print(sortString(str1))
+
